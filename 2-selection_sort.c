1af8376b7da3985f27a0492916ab510c5874e4a2
Lerato kgodumo <kgodumo345@gmail.com>
2023-11-20 02:17:37 -0800

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,61 +1,33 @@
 #include "sort.h"
 
 /**
- * swap - swap or switches the 2 given values.
- * @a: first int
- * @b: second int
- * Return: void.
- */
-void swap(int *a, int *b)
-{
-	int res;
-
-	res = *a;
-	*a = *b;
-	*b = res;
-}
-
-/**
- * get_min_index - finds the minimum index for a sub array.
- * @array: Pointer to array to sort
- * @start: Pointer to the starting index of the sub array.
- * @size: Pointer to the size of the array
- * Return: min_index.
- */
-size_t get_min_index(int *array, int start, size_t size)
-{
-	size_t n, min_index;
-	int min_val;
-
-	min_val = array[start];
-	min_index = start;
-	for (n = start + 1; n < size; n++)
-	{
-		if (array[n] < min_val)
-		{
-			min_val = array[n];
-			min_index = n;
-		}
-	}
-	return (min_index);
-}
-/**
- * selection_sort - sorts the array using the selection sort algorithm.
- * @array: Pointer to array to sort
- * @size: Pointr to the size of the array.
- * Return: void.
+ * selection_sort -  function that use selection sort algorithm
+ * @array: an array to sort
+ * @size: the size of the array
+ * Return: Nothing
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t n, min_index;
+	int aux = 0;
+	size_t n = 0, m = 0, pos = 0;
 
-	for (n = 0; n < size; n++)
+	if (array == NULL || size == 0)
+		return;
+
+	for (; n < size - 1; n++)
 	{
-		min_index = get_min_index(array, n, size);
-		if (n != min_index)
+		pos = n;
+		for (m = n + 1; m < size; m++)
 		{
-			swap(&array[n], &array[min_index]);
-			swap(&array[n], &array[min_index]);
+			if (array[m] < array[pos])
+				pos = m;
+		}
+		if (pos != n)
+		{
+			aux = array[n];
+			array[n] = array[pos];
+			array[pos] = aux;
+			print_array(array, size);
 		}
 	}
 }
